7817984be82faedeae22c568c832b7e1a1938702
Swiftminale <minalefetene@gmail.com>
2023-11-10 00:43:53 -0800

    first commit

---
+++ print_str.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+* _strlen - REturns the length of the string.
+* @str: String pointer.
+* Return: i.
+*/
+int _strlen( char *str)
+{
+int i;
+for (i = 0; str[i] != 0; i++)
+return(i);
+}
+/**
+* _strlenc - strlen function but applied for constan char pointer str
+* @str: char pointer
+* Return: i
+*/
+int _strlenc (const char *str)
+{
+int i;
+for (i = 0; str[i] != 0; i++)
+return (i)
+}
+

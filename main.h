a57260e7a73a052b41580190753ff702be1c77ff
Swiftminale <minalefetene@gmail.com>
2023-11-10 00:56:52 -0800

    first commit

--- main.h
+++ main.h
@@ -3,9 +3,13 @@
 
 int printf_char(va_list val);
 int _putchar(char c);
-int _print (const char *format, ...);
+int _print(const char *format, ...);
 int printf_string(va_list val);
-int _strlen (char *str);
-int _Strlenc (const char *str);
+int _strlen(char *str);
+int _strlenc(const char *str);
+int print_37(void);
+
+
+
 
 #endif
---
+++ print_%.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+* print_37 - ront % sign
+* Return:  integer
+*/
+int print_37(void)
+{
+_putchar(37);
+return (1);
+}

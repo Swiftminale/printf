b2af83a9e7fc956f241ce902ebfab6ff3fdfcfb8
Swiftminale <minalefetene@gmail.com>
2023-11-18 05:25:14 -0800

    first commit

--- printf_add.c
+++ printf_add.c
@@ -4,28 +4,27 @@ int printf_add() {
     int number = 42;
     float floatNumber = 3.14;
 
-    // + flag
     printf("Using + flag:\n");
-    printf("%+d\n", number); // Positive number with sign
-    printf("%+d\n", -number); // Negative number with sign
-    printf("%+f\n", floatNumber); // Positive float with sign
-    printf("%+f\n", -floatNumber); // Negative float with sign
+    printf("%+d\n", number);
+    printf("%+d\n", -number);
+    printf("%+f\n", floatNumber);
+    printf("%+f\n", -floatNumber);
     printf("\n");
 
-    // space flag
+
     printf("Using space flag:\n");
-    printf("% d\n", number); // Positive number with space
-    printf("% d\n", -number); // Negative number with sign
-    printf("% f\n", floatNumber); // Positive float with space
-    printf("% f\n", -floatNumber); // Negative float with sign
+    printf("% d\n", number);
+    printf("% d\n", -number);
+    printf("% f\n", floatNumber); 
+    printf("% f\n", -floatNumber);
     printf("\n");
 
-    // # flag
+
     printf("Using # flag:\n");
-    printf("%#o\n", number); // Octal with prefix
-    printf("%#x\n", number); // Hexadecimal with prefix
-    printf("%#e\n", floatNumber); // Scientific notation with decimal point
-    printf("%#g\n", floatNumber); // Shortest representation with decimal point
+    printf("%#o\n", number); 
+    printf("%#x\n", number);
+    printf("%#e\n", floatNumber); 
+    printf("%#g\n", floatNumber); 
     printf("\n");
 
     return 0;
